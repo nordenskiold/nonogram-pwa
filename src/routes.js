04f2b5130d97aa596bf88ea960f3f91033904a6f
nordenskiold <gabriel.nordenskiold@gmail.com>
2020-02-22 17:09:40 +0100

    Clean up boilerplate, discard unwanted features

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,12 @@
-import GuestRoutes from './pages/guest';
-import AuthRoutes from './pages/auth';
-import SplashScreen from './pages/splash';
+import GuestRoutes from "./pages/guest";
+import SplashScreen from "./pages/splash";
 
 export default class Routes {
   // eslint-disable-next-line
   apply(routeHandler) {
-    const routes = [
-      ...GuestRoutes,
-      ...AuthRoutes,
-      ...SplashScreen,
-    ];
+    const routes = [...GuestRoutes, ...SplashScreen];
 
-    routeHandler.hooks.initRoutes.tapPromise('AppRoutes', async () => {
+    routeHandler.hooks.initRoutes.tapPromise("AppRoutes", async () => {
       routeHandler.addRoutes(routes);
     });
   }
